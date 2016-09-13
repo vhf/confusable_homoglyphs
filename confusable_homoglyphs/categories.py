b76432feeba1d69a226315afc34112b5dfbd4a06
vhf <victorfelder@gmail.com>
2016-09-13 19:34:27 +0200

    Move generation to lib; fix tests

--- confusable_homoglyphs/categories.py
+++ confusable_homoglyphs/categories.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 import json
 import os
+import re
+from make_unicode import u
+
+try:
+    from urllib.request import urlopen
+
+    def get(url):
+        return urlopen(url).read().decode('utf-8').split('\n')
+except:
+    from urllib2 import urlopen as get
 
 
 def load():
@@ -50,3 +60,49 @@ def category(chr):
 def unique_aliases(string):
     cats = [alias(c) for c in string]
     return set(cats)
+
+
+def delete():
+    try:
+        os.remove('{}/categories.json'.format(os.getcwd()))
+    except OSError:
+        pass
+
+
+def generate():
+    # inspired by https://gist.github.com/anonymous/2204527
+    code_points_ranges = []
+    iso_15924_aliases = []
+    categories = []
+
+    match = re.compile(r'([0-9A-F]+)(?:\.\.([0-9A-F]+))?\W+(\w+)\s*#\s*(\w+)',
+                       re.UNICODE)
+
+    url = 'http://www.unicode.org/Public/UNIDATA/Scripts.txt'
+    file = get(url)
+    for line in file:
+        p = re.findall(match, line)
+        if p:
+            code_point_range_from, code_point_range_to, alias, category = p[0]
+            alias = u(alias.upper())
+            category = u(category)
+            if alias not in iso_15924_aliases:
+                iso_15924_aliases.append(alias)
+            if category not in categories:
+                categories.append(category)
+            code_points_ranges.append((
+                int(code_point_range_from, 16),
+                int(code_point_range_to or code_point_range_from, 16),
+                iso_15924_aliases.index(alias), categories.index(category))
+            )
+    code_points_ranges.sort()
+
+    categories_data = {
+        'iso_15924_aliases': iso_15924_aliases,
+        'categories': categories,
+        'code_points_ranges': code_points_ranges,
+    }
+
+    with open('{}/categories.json'.format(os.getcwd()), 'w+') as datafile:
+        json.dump(categories_data, datafile)
+    return True
